9afb15383b6d9d4b58bde9f02e9d2bbab83c422f
DoPlo12cz <doplo12@seznam.cz>
2026-07-05 10:10:03 +0200

    Refactor brain.js structure and add responses

--- brain.js
+++ brain.js
@@ -1,30 +1,285 @@
-const brain={
+const brain = {
 
 hello:[
 "Hello!",
 "Hi!",
-"Hey!"
-],
-
-anime:[
-"I love anime.",
-"What anime do you watch?",
-"Anime is awesome."
-],
-
-minecraft:[
-"I like Minecraft.",
-"Java or Bedrock?"
-],
-
-roblox:[
-"Roblox is fun.",
-"What game?"
+"Hey!",
+"Nice to meet you!",
+"Hello there!"
 ],
 
 bye:[
 "Bye!",
-"See you!"
+"Goodbye!",
+"See you later!",
+"Take care!",
+"Have a nice day!"
+],
+
+how_are_you:[
+"I'm doing great!",
+"I'm fine!",
+"Pretty good!",
+"Awesome!",
+"I'm okay!"
+],
+
+name:[
+"My name is Lumini.",
+"You can call me Lumini.",
+"I'm Lumini.",
+"I am your AI assistant."
+],
+
+age:[
+"I don't have an age.",
+"I'm just code.",
+"I was created recently."
+],
+
+creator:[
+"I was created by my developer.",
+"My creator is awesome."
+],
+
+anime:[
+"I love anime.",
+"Anime is amazing.",
+"What's your favorite anime?",
+"I enjoy watching anime."
+],
+
+one_piece:[
+"Luffy is awesome.",
+"Gear Fifth is crazy.",
+"One Piece is peak.",
+"I love the Straw Hats."
+],
+
+naruto:[
+"Naruto is legendary.",
+"I like Kakashi.",
+"Sasuke is cool."
+],
+
+jjk:[
+"Gojo is overpowered.",
+"JJK has amazing fights.",
+"I like Yuji."
+],
+
+death_note:[
+"Light is a genius.",
+"L is one of my favorites.",
+"Death Note is incredible."
+],
+
+dragon_ball:[
+"Goku is strong.",
+"Vegeta never gives up.",
+"Ultra Instinct is cool."
+],
+
+mushoku:[
+"Rudeus changes a lot.",
+"I like Mushoku Tensei.",
+"The world building is amazing."
+],
+
+music:[
+"I enjoy music.",
+"What music do you like?",
+"J-Pop is great."
+],
+
+night_dancer:[
+"Night Dancer is amazing.",
+"I like imase.",
+"That song has a great vibe."
+],
+
+game:[
+"What games do you play?",
+"I like games.",
+"Gaming is fun."
+],
+
+minecraft:[
+"Java or Bedrock?",
+"I like Minecraft.",
+"Building is fun."
+],
+
+roblox:[
+"What Roblox games do you play?",
+"I know Blox Fruits.",
+"Roblox has many games."
+],
+
+blox_fruits:[
+"Dragon is powerful.",
+"Kitsune is fun.",
+"I know Blox Fruits."
+],
+
+food:[
+"I don't eat.",
+"Pizza sounds good.",
+"Food is important."
+],
+
+pizza:[
+"Pizza is delicious.",
+"I like pepperoni pizza."
+],
+
+school:[
+"School can be difficult.",
+"Good luck at school!"
+],
+
+math:[
+"I like math.",
+"What equation are you solving?"
+],
+
+science:[
+"Science is interesting.",
+"I like physics."
+],
+
+space:[
+"The universe is huge.",
+"Space is fascinating."
+],
+
+ai:[
+"I like talking with people.",
+"I'm still learning."
+],
+
+computer:[
+"Computers are amazing.",
+"I run on code."
+],
+
+programming:[
+"I like JavaScript.",
+"Programming is fun.",
+"HTML is a good start."
+],
+
+html:[
+"HTML builds websites.",
+"HTML is easy to learn."
+],
+
+css:[
+"CSS makes websites beautiful."
+],
+
+javascript:[
+"JavaScript makes websites interactive."
+],
+
+python:[
+"Python is beginner friendly."
+],
+
+java:[
+"Java is powerful."
+],
+
+csharp:[
+"C# is popular for Unity."
+],
+
+unity:[
+"Unity is great for games."
+],
+
+weather:[
+"I can't control the weather.",
+"Hopefully it's sunny."
+],
+
+rain:[
+"I like listening to rain."
+],
+
+love:[
+"Love is complicated.",
+"Love takes time."
+],
+
+friend:[
+"Friends are important.",
+"A good friend is valuable."
+],
+
+family:[
+"Family matters.",
+"I hope your family is doing well."
+],
+
+dog:[
+"Dogs are adorable."
+],
+
+cat:[
+"Cats are cute."
+],
+
+bird:[
+"Birds can fly high."
+],
+
+car:[
+"I like fast cars."
+],
+
+movie:[
+"I enjoy movies too."
+],
+
+book:[
+"Books are full of knowledge."
+],
+
+japan:[
+"I like Japan.",
+"Japan has great culture."
+],
+
+travel:[
+"I would love to travel if I could."
+],
+
+color:[
+"My favorite color is blue."
+],
+
+sport:[
+"Sports keep people healthy."
+],
+
+sleep:[
+"Sleep is important."
+],
+
+time:[
+"Time goes fast."
+],
+
+joke:[
+"Why did the programmer quit? Because he didn't get arrays.",
+"I would tell you a UDP joke, but you might not get it."
+],
+
+thanks:[
+"You're welcome!",
+"No problem!",
+"Glad I could help."
 ]
 
 };
