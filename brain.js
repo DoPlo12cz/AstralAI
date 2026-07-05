d7f7145e4bcbbd5777ddc33fe73d11c6b7329387
DoPlo12cz <doplo12@seznam.cz>
2026-07-05 10:04:43 +0200

    Create brain.js with response categories
    Added a brain object with predefined responses for various topics.

---
+++ brain.js
@@ -0,0 +1,30 @@
+const brain={
+
+hello:[
+"Hello!",
+"Hi!",
+"Hey!"
+],
+
+anime:[
+"I love anime.",
+"What anime do you watch?",
+"Anime is awesome."
+],
+
+minecraft:[
+"I like Minecraft.",
+"Java or Bedrock?"
+],
+
+roblox:[
+"Roblox is fun.",
+"What game?"
+],
+
+bye:[
+"Bye!",
+"See you!"
+]
+
+};
